01bf8940f36828a0466f815477ec5d7086126c84
Kuuhaku_kazari <SP_YYY@outlook.com>
2025-02-23 15:55:49 +0800

    优化图像路径，重构图像加载逻辑，新增练习页面

--- scripts.js
+++ scripts.js
@@ -1,33 +1,14 @@
 // 定义图像路径的宏
-const imageBasePath = "mat/imgs/";
+const imageBasePath = "resources/anime_pictures/"; // 图像基础路径
 const imageCount = 34;
 const switchInterval = 100; // 切换间隔时间（毫秒）
 const audiopath = "D:/KH_music";
-
-// 动态生成图像元素
-// window.onload = () => {
-//     const container = document.getElementById("image-container");
-
-//     for (let i = 1; i <= imageCount; i++) {
-//         // 创建一个 img 元素用于显示图像
-//         const img = document.createElement("img");
-//         // 用于格式化数字，使其始终为 4 位数
-//         const paddedIndex = String(i).padStart(4, "0");
-
-//         // 尝试加载 .PNG 文件
-//         img.src = `${imageBasePath}${paddedIndex}.PNG`;
-//         img.alt = `画像${i}`;
-//         img.style.width = "300px";
-//         img.style.height = "300px";
-
-//         container.appendChild(img);
-//     }
-// };
 
 // 快速随机切换图像
 // 动态生成图像元素
 window.onload = () =>
 {
+    // 获取图像容器元素
     const container = document.getElementById("image-container");
 
     // 创建一个 img 元素用于显示随机图像
@@ -43,7 +24,8 @@ window.onload = () =>
         const paddedIndex = String(randomIndex).padStart(4, "0");
 
         // 尝试加载 .PNG 文件，如果失败则加载 .jpg 文件
-        img.src = `${imageBasePath} ${paddedIndex}.png`;
+        let src = `${imageBasePath}${paddedIndex}.png`;
+        img.src = src;
         img.alt = `画像${randomIndex}`;
     }
 
